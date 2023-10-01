4725ec476cba662e809941fd0a4d87d1be3fc033
Nihal Developer <nihaltmdev@gmail.com>
2023-10-02 00:25:30 +0530

    added remaining entity tables to db and created srs doc file

--- hms.sql
+++ hms.sql
@@ -47,6 +47,59 @@ LOCK TABLES `appointment` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `billing`
+--
+
+DROP TABLE IF EXISTS `billing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `billing` (
+  `id` int NOT NULL,
+  `patient` int NOT NULL,
+  `billing_date` datetime NOT NULL,
+  `amount` float NOT NULL,
+  `payment_status` tinyint(1) NOT NULL,
+  `details` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `billing`
+--
+
+LOCK TABLES `billing` WRITE;
+/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `department` (
+  `name` varchar(30) NOT NULL,
+  `head_id` int DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`name`),
+  KEY `head_id` (`head_id`),
+  CONSTRAINT `department_ibfk_1` FOREIGN KEY (`head_id`) REFERENCES `doctor` (`doctor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `doctor`
 --
 
@@ -127,6 +180,65 @@ LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `prescription`
+--
+
+DROP TABLE IF EXISTS `prescription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prescription` (
+  `id` int NOT NULL,
+  `patient` int NOT NULL,
+  `doctor` int NOT NULL,
+  `prescription_date` datetime NOT NULL,
+  `medications` varchar(100) NOT NULL,
+  `instructions` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `patient` (`patient`),
+  KEY `doctor` (`doctor`),
+  CONSTRAINT `prescription_ibfk_1` FOREIGN KEY (`patient`) REFERENCES `patient` (`patient_id`),
+  CONSTRAINT `prescription_ibfk_2` FOREIGN KEY (`doctor`) REFERENCES `doctor` (`doctor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prescription`
+--
+
+LOCK TABLES `prescription` WRITE;
+/*!40000 ALTER TABLE `prescription` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prescription` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ward`
+--
+
+DROP TABLE IF EXISTS `ward`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ward` (
+  `ward_id` int NOT NULL,
+  `ward_type` varchar(20) NOT NULL,
+  `patient` int DEFAULT NULL,
+  `capacity` int NOT NULL,
+  `availability` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ward_id`),
+  KEY `patient` (`patient`),
+  CONSTRAINT `ward_ibfk_1` FOREIGN KEY (`patient`) REFERENCES `patient` (`patient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ward`
+--
+
+LOCK TABLES `ward` WRITE;
+/*!40000 ALTER TABLE `ward` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ward` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -137,4 +249,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-01 18:31:30
+-- Dump completed on 2023-10-02  0:23:45
