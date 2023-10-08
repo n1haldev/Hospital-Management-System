0700d7d8928da422a1a6339c51240a865cf404aa
Nihal Developer <nihaltmdev@gmail.com>
2023-10-08 21:49:57 +0530

    half of SRS is done

--- hms.sql
+++ hms.sql
@@ -1,4 +1,5 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Linux (x86_64)
+Terminal close -- exit!
+trib 8.0.34, for Linux (x86_64)
 --
 -- Host: localhost    Database: hms
 -- ------------------------------------------------------
