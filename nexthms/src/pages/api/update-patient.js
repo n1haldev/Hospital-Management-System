4280b109073709974d47c7780fb1f566ea03956a
n1haldev <nihaltmdev@gmail.com>
2023-11-06 20:47:57 +0530

    update feature working

---
+++ nexthms/src/pages/api/update-patient.js
@@ -0,0 +1,36 @@
+import mysql from 'mysql2/promise';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const formData = JSON.parse(req.body);
+
+    try {
+      // Create a MySQL database connection
+      const connection = await mysql.createConnection({
+        // host: 'your_database_host',
+        user: 'root',
+        password: 'Il0vemym0m?',
+        database: 'Hms',
+      });
+
+      const [rows] = await connection.execute(
+        'UPDATE patient SET name = ?, gender = ?, contact = ?, insurance = ? WHERE patient_id = ?', [
+            formData.name,
+            formData.gender,
+            formData.contact,
+            formData.insurance,
+            formData.id,
+        ]
+      );
+
+      await connection.end();
+
+      res.status(200).json({ message: 'Patient registered successfully' });
+    } catch (error) {
+      console.error('Error registering patient:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  } else {
+    res.status(405).end();
+  }
+}
--- nexthms/src/pages/patient/update.js
+++ nexthms/src/pages/patient/update.js
@@ -3,40 +3,66 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 export default function Home() {
+  const initialFormData = {
+    id: '',
+    name: '',
+    gender: '',
+    dob: '',
+    contact: '',
+    insurance: '',
+  };
+
   const [searchID, setSearchID] = useState('');
   const [patients, setPatients] = useState([]);
+  const [formData, setFormData] = useState(initialFormData);
 
   const handleSearch = async () => {
-    // Fetch patients with matching names from the server
+    // Fetch patients with matching IDs from the server
     const response = await fetch(`/api/get-patients?id=${searchID}`);
     if (response.ok) {
       const data = await response.json();
-      setPatients(data);
+      if (data.length > 0) {
+        // If patient data is found, set the ID and load the first patient or reset the form
+        setPatients(data);
+        setFormData({
+          ...data[0],
+          id: searchID,
+        });
+      } else {
+        // No patient found, reset the form
+        setPatients([]);
+        setFormData(initialFormData);
+      }
+    } else {
+      setPatients([]);
+      setFormData(initialFormData);
+      alert("No patient with patient ID " + searchID + " found")
     }
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e, patient) => {
     e.preventDefault();
 
-    // Send the data to your server to store in the MySQL database
-    const response = await fetch('/api/register-patient', {
+    // Send the updated data to your server to update the patient's information
+    const response = await fetch('/api/update-patient', {
       method: 'POST',
-      body: JSON.stringify({ ...formData }),
+      body: JSON.stringify(formData),
     });
 
     if (response.ok) {
-      // Handle success, e.g., show a success message or redirect to a thank you page
-      alert('Patient registered successfully');
-      // clearing form elements
-      setFormData(initialFormData)
+      // Handle success, e.g., show a success message or update the UI
+      alert('Patient information updated successfully');
     } else {
       // Handle errors
-      alert('Patient registration failed');
+      alert('Failed to update patient information');
     }
   };
 
@@ -55,81 +81,79 @@ export default function Home() {
           <button onClick={handleSearch}>Search</button>
         </div>
 
-        {/* <ul>
-          {patients.map((patient) => (
-            <li key={patient.id}>
-              {patient.name} - Patient ID: {patient.id}
-              <Link href={`/edit-patient?id=${patient.id}`}>Edit</Link>
-            </li>
-          ))}
-        </ul> */}
-
-
-        {patients.map((patient => (
-            <div>
-                <h1 className="text-4xl text-center pt-2 font-bold">Patient Registration</h1>
-                <div className="items-center p-8">
-                    <form onSubmit={handleSubmit}>
-                        <fieldset className="border border-green-500 p-4">
-                            <legend className="p-2 font-bold">Patient Details</legend>
-                            <label htmlFor='name'>Name: </label>
-                            <input
-                                type='text'
-                                id='name'
-                                name='name'
-                                value={patient.name}
-                                onChange={handleChange} />
-                            <br />
-                            <br />
-                            <div>
-                            <label>Gender:</label>
-                            <input
-                                type="radio"
-                                name="gender"
-                                value="Male"
-                                checked={patient.gender === 'Male'}
-                                onChange={handleChange}
-                            /> Male
-                            <input
-                                type="radio"
-                                name="gender"
-                                value="Female"
-                                checked={patient.gender === 'Female'}
-                                onChange={handleChange}
-                            /> Female
-                            </div>
-                            <br />
-                            <br />
-                            <label htmlFor='dob'>Date of Birth: </label>
-                            <input
-                                type='date'
-                                id='dob'
-                                name='dob'
-                                value={patient.dob}
-                                onChange={handleChange} />
-                            <br />
-                            <br />
-                            <label htmlFor='contactInfo'>Contact Information: </label>
-                            <input
-                                type='text'
-                                id='contactInfo'
-                                name='contactInfo'
-                                value={patient.contact}
-                                onChange={handleChange} />
-                            <br />
-                            <br />
-                            <label htmlFor='insuranceInfo'>Insurance Information: </label>
-                            <br />
-                            <textarea placeholder='Enter patient insurance information' rows={4} cols={40} name="insuranceInfo" value={patient.insurance} onChange={handleChange}></textarea>
-                            <br/>
-                            <button type="submit">Save Changes</button>
-                        </fieldset>
-                    </form>
-                </div>
+        {patients.map((patient) => (
+          <div key={patient.id}>
+            <h1 className="text-4xl text-center pt-2 font-bold">Patient Registration</h1>
+            <div className="items-center p-8">
+              <form onSubmit={(e) => handleSubmit(e, patient)}>
+                <fieldset className="border border-green-500 p-4">
+                  <legend className="p-2 font-bold">Patient Details</legend>
+                  <label htmlFor="name">Name:</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                  />
+                  <br />
+                  <br />
+                  <label htmlFor="gender">Gender:</label>
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="Male"
+                    checked={formData.gender === 'Male'}
+                    onChange={handleChange}
+                  />{' '}
+                  Male
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="Female"
+                    checked={formData.gender === 'Female'}
+                    onChange={handleChange}
+                  />{' '}
+                  Female
+                  <br />
+                  <br />
+                  <label htmlFor="dob">Date of Birth:</label>
+                  <input
+                    type="date"
+                    id="dob"
+                    name="dob"
+                    value={formData.dob}
+                    onChange={handleChange}
+                  />
+                  <br />
+                  <br />
+                  <label htmlFor="contact">Contact Information:</label>
+                  <input
+                    type="text"
+                    id="contact"
+                    name="contact"
+                    value={formData.contact}
+                    onChange={handleChange}
+                  />
+                  <br />
+                  <br />
+                  <label htmlFor="insurance">Insurance Information:</label>
+                  <br />
+                  <textarea
+                    placeholder="Enter patient insurance information"
+                    rows={4}
+                    cols={40}
+                    name="insurance"
+                    value={formData.insurance}
+                    onChange={handleChange}
+                  ></textarea>
+                  <br />
+                  <button type="submit">Save Changes</button>
+                </fieldset>
+              </form>
             </div>
-        )))}
-
-
+          </div>
+        ))}
       </div>
     </div>
   );
